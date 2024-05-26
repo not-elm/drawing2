b99878d30729f73726116b56dc29d867548e596d
Yuichiro Kikura <y.kikura@gmail.com>
2024-05-27 00:46:22 +0900

    Apply lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { css } from "@emotion/react";
 import type { FC } from "react";
-import {css} from "@emotion/react";
 
 export const App: FC = () => {
-	return <h1 css={css`
+	return (
+		<h1
+			css={css`
 		color: red;
 		background-color: black;
-	`}>Hello World</h1>;
+	`}
+		>
+			Hello World
+		</h1>
+	);
 };
