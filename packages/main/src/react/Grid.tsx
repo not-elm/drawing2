b022d95b6ff9701849c3b06447744ef55db0af98
Yuichiro Kikura <y.kikura@gmail.com>
2024-10-08 23:14:04 +0900

    Implement Grid

---
+++ packages/main/src/react/Grid.tsx
@@ -0,0 +1,76 @@
+import { useMemo } from "react";
+import { useController } from "./ControllerProvider";
+import { useStore } from "./hooks/useStore";
+
+export function Grid() {
+    const controller = useController();
+    const viewport = useStore(controller.viewportStore);
+
+    const canvas = useMemo(() => document.createElement("canvas"), []);
+
+    const tileImageUrl = useMemo(
+        () => generateGridTile(canvas, viewport.scale),
+        [canvas, viewport.scale],
+    );
+    return (
+        <div
+            css={{
+                position: "absolute",
+                inset: 0,
+                backgroundImage: `url(${tileImageUrl})`,
+                backgroundPosition: `${
+                    (Math.ceil(viewport.x / TILE_SIZE) * TILE_SIZE -
+                        viewport.x) *
+                    viewport.scale
+                }px ${
+                    (Math.ceil(viewport.y / TILE_SIZE) * TILE_SIZE -
+                        viewport.y) *
+                    viewport.scale
+                }px`,
+                backgroundRepeat: "repeat",
+            }}
+        />
+    );
+}
+
+const TILE_SIZE = 256;
+
+function generateGridTile(canvas: HTMLCanvasElement, scale: number): string {
+    const WIDTH = TILE_SIZE * scale;
+    const HEIGHT = TILE_SIZE * scale;
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    canvas.style.imageRendering = "pixelated";
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+        throw new Error("Could not get 2d context");
+    }
+
+    const logScale = Math.log2(scale) / Math.log2(4);
+
+    let gridSize = 4;
+    for (let i = 0; i < 3; i++) {
+        const bestLogScale = 1 - i;
+        ctx.fillStyle = `rgba(0, 0, 0, ${Math.max(
+            0,
+            (-3 / 2) * Math.abs(logScale - bestLogScale) + 1,
+        )})`;
+        for (let x = 0; x <= TILE_SIZE; x += gridSize) {
+            for (let y = 0; y <= TILE_SIZE; y += gridSize) {
+                const circle = new Path2D();
+                circle.arc(
+                    Math.round(x * scale),
+                    Math.round(y * scale),
+                    1,
+                    0,
+                    2 * Math.PI,
+                );
+                ctx.fill(circle);
+            }
+        }
+        gridSize *= 4;
+    }
+
+    return canvas.toDataURL("image/bmp");
+}
